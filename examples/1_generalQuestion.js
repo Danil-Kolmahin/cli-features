e9fd73d72e5da1b6ceaf7b40a0490c3830fa45e0
Kolmagin Danil <dkolmagin@gmail.com>
2021-05-31 11:52:00 +0300

    Added minLength checker, input validation, errors when validation not success

--- examples/1_generalQuestion.js
+++ examples/1_generalQuestion.js
@@ -15,8 +15,14 @@ const Questioner = require('../lib/questioner/questioner');
   result.age = await question('What is your age?', {
     possibleChars: [...'0123456789'],
   });
+  result.age = await question('What is your phone number?', {
+    possibleChars: [...'+-()0123456789'],
+    validate: /^[+]?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/im,
+    validateErrMessage: "Error: it isn't phone number",
+  });
   result.password = await question('Your password:', {
     passMode: true,
+    minLength: 6,
   });
 
   console.log(result);
